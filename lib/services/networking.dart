d01a0b8773ea29c54a803fb29bc47fbf4bf58612
Monasobhy-debug <nona.lover2045@gmail.com>
2021-07-16 20:34:09 +0200

    flutter

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -3,14 +3,21 @@ import 'dart:convert';
 
 class NetworkHelper {
   NetworkHelper(this.url);
+
   final Uri url;
+  var data;
 
   Future getData() async {
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(jsonDecode(data));
-    } else
-      print(response.statusCode);
+    try {
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        data = jsonDecode(response.body);
+      } else
+        print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
+
+    return data;
   }
 }
